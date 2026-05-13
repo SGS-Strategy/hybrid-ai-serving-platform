3e8be6cd17fc961bd2bb2480cbfc27b266599ae1
kimbirdwon <pswk0907@gmail.com>
2026-05-13 17:15:19 +0900

    terraform 뼈대 구성2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -35,3 +35,66 @@ variable "private_subnet_cidrs" {
     "10.0.13.0/24",
   ]
 }
+
+variable "ecr_repositories" {
+  description = "ECR repository names"
+  type        = list(string)
+  default = [
+    "factory-simulator",
+    "operation-simulator",
+  ]
+}
+
+variable "eks_cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+  default     = "1.31"
+}
+
+variable "eks_node_instance_types" {
+  description = "Instance types for the EKS managed node group"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
+
+variable "eks_node_desired_size" {
+  description = "Desired number of nodes in the EKS managed node group"
+  type        = number
+  default     = 2
+}
+
+variable "eks_node_min_size" {
+  description = "Minimum number of nodes in the EKS managed node group"
+  type        = number
+  default     = 1
+}
+
+variable "eks_node_max_size" {
+  description = "Maximum number of nodes in the EKS managed node group"
+  type        = number
+  default     = 3
+}
+
+variable "msk_kafka_version" {
+  description = "Apache Kafka version for the MSK cluster"
+  type        = string
+  default     = "3.9.x"
+}
+
+variable "msk_broker_instance_type" {
+  description = "Broker instance type for the MSK cluster"
+  type        = string
+  default     = "kafka.t3.small"
+}
+
+variable "msk_number_of_broker_nodes" {
+  description = "Number of broker nodes for the MSK cluster"
+  type        = number
+  default     = 3
+}
+
+variable "msk_ebs_volume_size" {
+  description = "Broker EBS volume size in GiB for the MSK cluster"
+  type        = number
+  default     = 1000
+}
