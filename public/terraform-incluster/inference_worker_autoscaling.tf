b51ed0e94abee79489bc3942e84d51bb9eb14000
kimbirdwon <pswk0907@gmail.com>
2026-07-14 19:07:33 +0900

    fix: inference-worker KEDA ScaledObject를 Terraform 자동 생성으로 전환

---
+++ public/terraform-incluster/inference_worker_autoscaling.tf
@@ -0,0 +1,40 @@
+resource "kubernetes_manifest" "inference_worker_scaledobject" {
+  manifest = {
+    apiVersion = "keda.sh/v1alpha1"
+    kind       = "ScaledObject"
+    metadata = {
+      name      = "inference-worker"
+      namespace = kubernetes_namespace.inference.metadata[0].name
+      labels = {
+        "argocd.argoproj.io/instance" = "inference-worker"
+      }
+    }
+    spec = {
+      scaleTargetRef = {
+        name = "inference-worker"
+      }
+      minReplicaCount = 2
+      maxReplicaCount = 6
+      pollingInterval = 15
+      cooldownPeriod  = 120
+      triggers = [
+        {
+          type = "kafka"
+          metadata = {
+            bootstrapServers = data.terraform_remote_state.platform.outputs.msk_bootstrap_brokers
+            consumerGroup    = "inference-worker-group"
+            topic            = "inference-request"
+            lagThreshold     = "20"
+            offsetResetPolicy = "latest"
+            tls              = "enable"
+          }
+        }
+      ]
+    }
+  }
+
+  depends_on = [
+    kubernetes_namespace.inference,
+    kubernetes_config_map.inference_config,
+  ]
+}
